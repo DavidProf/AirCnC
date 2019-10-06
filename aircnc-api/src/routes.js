9d227190558c7441ad059c2b686b0f7457ef49c9
DavidProf <david.lima50@etec.sp.gov.br>
2019-10-06 12:41:33 -0300

    aircnc-api ok

--- aircnc-api/src/routes.js
+++ aircnc-api/src/routes.js
@@ -1,10 +1,29 @@
-const express = require("express");
+const express = require('express');
+//images
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+//
+const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
+
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
 routes.get('/', (req, res) => {
     res.send('api');
 });
 
+routes.post('/sessions', SessionController.store);
+
+routes.get('/spots', SpotController.index);
+//image
+routes.post('/spots', upload.single('thumbnail'),SpotController.store)
+
+routes.get('/dashboard', DashboardController.show);
+
+routes.post('/spots/:spot_id/bookings',BookingController.store);
 
 module.exports = routes;
